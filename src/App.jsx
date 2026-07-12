1ac57cc48c8c72159269acb5559622877f978668
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-07-12 14:17:04 +0900

    add:markup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,35 @@ function App() {
 
   return (
     <>
+    <section class="input-area">
+      <input id="todo-input" type="text" placeholder="TODOを入力してください。"/>
+      <button id="add-button">追加</button>
+    </section>
+
+    <section class="incomplete-area">
+      <p class="todo-title">未完了のTODO</p>
+      <ul id="incomplete-list">
+        <li>
+          <div className="todo-list">
+            <p>Todoアイテム</p>
+            <button className="button-style">削除</button>
+            <button className="button-style">完了</button>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <section class="complete-area bg-color">
+      <p class="todo-title">完了のTODO</p>
+      <ul id="complete-list">
+        <li>
+          <div className="todo-list">
+            <p>Todoアイテム</p>
+            <button className="button-style">戻す</button>
+          </div>
+        </li>
+      </ul>
+    </section>
     
     </>
   )
